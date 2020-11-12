78c925e5e98feb49262effd626de1cf45ed1951a
Zer0Power <74372329+Zer0Power@users.noreply.github.com>
2020-11-12 20:55:02 +0330

    Create Config.js

---
+++ Files/Config.js
@@ -0,0 +1,9 @@
+//---------------------------🌿 ᶻᵉʳᵒ༄ᴩᴏᴡᴇʀ 💀---------------------------//
+module.exports = {
+    token: '', //Set Your Bot Token
+    clockchannel: '', //Set Your Channel ID
+    timezone: 'Asia/Tehran', //Set Your Time Zone
+    format: 'HH:mm', //Set Your Favorite Form => https://momentjs.com/docs/#/displaying/format/
+    updateinterval: 600000, // Discord is ratelimiting us for 10 minutes!
+  }
+//---------------------------🌿 ᶻᵉʳᵒ༄ᴩᴏᴡᴇʀ 💀---------------------------//
